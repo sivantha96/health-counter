265dba18a1272a6172d3d720c9e6514a51c72491
Sivantha Paranavithana <madusham3@gmail.com>
2020-04-20 14:49:57 +0530

    Update naming conventions

--- frontend/src/app/component/bucket/bucket.component.ts
+++ frontend/src/app/component/bucket/bucket.component.ts
@@ -3,7 +3,6 @@ import { transferArrayItem } from '@angular/cdk/drag-drop';
 import { PostData } from '../../models/bucket';
 import { ActivatedRoute } from '@angular/router';
 import { MatStepper } from '@angular/material/stepper';
-import {MatProgressBarModule} from '@angular/material/progress-bar'; 
 
 @Component({
   selector: 'app-bucket',
@@ -11,44 +10,57 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
   styleUrls: ['./bucket.component.css'],
 })
 export class BucketComponent implements OnInit {
-
+  // Data of a single person - For POST Req
   postData: PostData;
-  members: any[]
 
-  bucketIndex: number
-  noOfBuckets: number
+  // Array for number of family members
+  members: any[];
 
-  symptomNo: number;
+  // Current bucket - starts from Zero 0
+  indexBucket: number;
 
-  // array for holding carousel items
-  symptomsTemplate: any[] = [
+  // No of family members
+  noOfBuckets: number;
+
+  // Current symptom number
+  indexCarousel: number;
+
+  // Template f severities to copy into symptoms array when creating a new step(bucket)
+  carouselTemplate: any[] = [
+    ['Female', 'Male', 'Other'],
+    ['Infant', 'Child', 'Teenager', 'Adult', 'Elderly'],
     ['No symptoms', 'Mild symptoms', 'Severe symptoms'],
     ['No symptoms', 'Mild symptoms', 'Severe symptoms'],
     ['No symptoms', 'Mild symptoms', 'Severe symptoms'],
   ];
 
+  // Array for holding dropped symptoms for the current bucket
+  currentBucket: string[];
 
-  bucket: string[];
-  bucketStates: any[]
+  // Array for hold buckets
+  bucketStates: any[];
 
-  symptomsArray: any;
-  symptomsSates: any[]
+  // Array for hold the severities of the current symptoms
+  carouselArray: any;
 
-  progressValue=0
-  progressStepCost=0
-  
+  // Array for hold the state of carouselArray
+  carouselStates: any[];
+
+  // current value of the progress
+  progressValue = 0;
+
+  // value of a single step
+  progressStepCost = 0;
 
   constructor(private route: ActivatedRoute) {
-    this.symptomNo = 0;
-    this.bucketIndex = 0;
-
-    this.symptomsSates = []
-
-    this.bucketStates = []
+    this.indexCarousel = 0;
+    this.indexBucket = 0;
+    this.carouselStates = [];
+    this.bucketStates = [];
   }
 
   ngOnInit(): void {
-    // parse stringified data received from the previous route
+    // parsing stringified data received from the previous route to postData
     this.route.queryParams.subscribe((params) => {
       try {
         this.postData = { ...JSON.parse(params.postData) };
@@ -57,31 +69,39 @@ export class BucketComponent implements OnInit {
       }
     });
 
-    this.noOfBuckets = +this.postData.family_members
+    // Setting number of buckets according to the received number of family members
+    this.noOfBuckets = +this.postData.family_members;
 
-    this.members = this.range(this.noOfBuckets)
+    // constructing the dummy array for stepper
+    this.members = this.range(this.noOfBuckets);
 
-    this.progressStepCost=100/this.noOfBuckets;
-    this.progressValue=this.progressStepCost
-    this.symptomsSates[0] = JSON.parse(JSON.stringify(this.symptomsTemplate))
-    this.bucketStates[0] = []
+    // calculate the value for a single step
+    this.progressStepCost = 100 / this.noOfBuckets;
 
-    this.symptomsArray = this.symptomsSates[0]
-    this.bucket = this.bucketStates[0];
+    // setting the staring value of the progress
+    this.progressValue = this.progressStepCost;
 
+    // deep copy
+    this.carouselStates[0] = JSON.parse(JSON.stringify(this.carouselTemplate));
 
+    // initialize
+    this.bucketStates[0] = [];
+
+    this.carouselArray = this.carouselStates[0];
+
+    this.currentBucket = this.bucketStates[0];
   }
 
-  //range for *ngFor
+  // Range for *ngFor in Template
   range(n: number): any[] {
     return Array(n);
   }
 
   drop(event: any) {
-    if (event.previousContainer === event.container) {
-      // console.log(event);
-    } else {
-      // console.log(event);
+    if (event.previousContainer !== event.container) {
+      console.log(event);
+      console.log(this.indexCarousel)
+      console.log(event.item.element.nativeElement.textContent);
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
@@ -89,80 +109,63 @@ export class BucketComponent implements OnInit {
         event.currentIndex
       );
 
-      this.symptomsSates[this.bucketIndex] = JSON.parse(JSON.stringify(this.symptomsArray))
-      this.bucketStates[this.bucketIndex] = JSON.parse(JSON.stringify(this.bucket))
-
+      // Save the template
+      this.carouselStates[this.indexBucket] = JSON.parse(
+        JSON.stringify(this.carouselArray)
+      );
+      this.bucketStates[this.indexBucket] = JSON.parse(
+        JSON.stringify(this.currentBucket)
+      );
     }
-
-    // this.bucket = [];
   }
 
+  // Carousel arrow click Next & Back
   onClick(button) {
     if (button === 'Previous') {
-      if (this.symptomNo === 0) {
-        this.symptomNo = this.symptomsArray.length;
+      if (this.indexCarousel === 0) {
+        this.indexCarousel = this.carouselArray.length;
       }
-      this.symptomNo = this.symptomNo - 1;
+      this.indexCarousel = this.indexCarousel - 1;
     } else {
-      this.symptomNo = this.symptomNo + 1;
-      if (this.symptomNo === this.symptomsArray.length) {
-        this.symptomNo = 0;
+      this.indexCarousel = this.indexCarousel + 1;
+      if (this.indexCarousel === this.carouselArray.length) {
+        this.indexCarousel = 0;
       }
     }
   }
 
-  //navigation
+  // Next of Stepper - Next bucket
   goForward(stepper: MatStepper) {
-    if (this.bucketIndex < this.noOfBuckets-1) {  
-      if (this.symptomsSates[this.bucketIndex + 1] === undefined) {
-        console.log('undefined')
-        this.symptomsSates[this.bucketIndex + 1] = JSON.parse(JSON.stringify(this.symptomsTemplate))
-        this.bucketStates[this.bucketIndex + 1] = []
-
-        
-       
-
-
+    if (this.indexBucket < this.noOfBuckets - 1) {
+      if (this.carouselStates[this.indexBucket + 1] === undefined) {
+        console.log('undefined');
+        this.carouselStates[this.indexBucket + 1] = JSON.parse(
+          JSON.stringify(this.carouselTemplate)
+        );
+        this.bucketStates[this.indexBucket + 1] = [];
       }
-
-      this.symptomsArray = JSON.parse(JSON.stringify(this.symptomsSates[++this.bucketIndex])) 
-      this.bucket = JSON.parse(JSON.stringify(this.bucketStates[this.bucketIndex])) 
-      console.log('before state - ' + this.symptomsSates[this.bucketIndex])
+      this.carouselArray = JSON.parse(
+        JSON.stringify(this.carouselStates[++this.indexBucket])
+      );
+      this.currentBucket = JSON.parse(
+        JSON.stringify(this.bucketStates[this.indexBucket])
+      );
+      console.log('before state - ' + this.carouselStates[this.indexBucket]);
       stepper.next();
-
-      this.progressValue+=this.progressStepCost
-      console.log(this.progressValue)
-      // console.log('bucketNo ' + (this.bucketIndex+1))
-
-
-
-      // console.log(this.symptomsSates[this.bucketIndex]) 
-      
-
-      
-      // this.symptomsSates.forEach((state) => {
-      //   console.log(state)
-      // })
+      this.progressValue += this.progressStepCost;
+      console.log(this.progressValue);
     }
-
-
   }
 
+  // Back of Stepper - Previous bucket
   goBack(stepper: MatStepper) {
-    if (this.bucketIndex > 0) {
-      this.symptomsArray = this.symptomsSates[--this.bucketIndex]
-      this.bucket = this.bucketStates[this.bucketIndex]
-      console.log('bucketNo ' + this.bucketIndex)
+    if (this.indexBucket > 0) {
+      this.carouselArray = this.carouselStates[--this.indexBucket];
+      this.currentBucket = this.bucketStates[this.indexBucket];
+      console.log('bucketNo ' + this.indexBucket);
       stepper.previous();
-
-      this.progressValue-=this.progressStepCost
-      console.log(this.progressValue)
+      this.progressValue -= this.progressStepCost;
+      console.log(this.progressValue);
     }
-
   }
-
-
-
-
-
 }
