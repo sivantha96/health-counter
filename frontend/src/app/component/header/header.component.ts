2896e3b51d9710e4b207aadc6e25a784a32ab028
Sivantha Paranavithana <madusham3@gmail.com>
2020-05-02 16:15:39 +0530

    Add styling to header

--- frontend/src/app/component/header/header.component.ts
+++ frontend/src/app/component/header/header.component.ts
@@ -3,6 +3,8 @@ import { TranslateService } from '@ngx-translate/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { MatMenuTrigger } from '@angular/material/menu';
 
+declare var $: any;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -13,34 +15,39 @@ export class HeaderComponent implements OnInit {
 
   selectedLang: string;
   currentLan: string;
+  timer;
 
-  constructor(public translate: TranslateService, private overlayContainer: OverlayContainer) {
+  constructor(
+    public translate: TranslateService,
+    private overlayContainer: OverlayContainer,
+  ) {
     translate.addLangs(['en', 'si', 'ta']);
     translate.setDefaultLang('en');
   }
 
-
   ngOnInit(): void {
-    this.currentLan = "English";
+    this.currentLan = 'English';
   }
 
   toggleLanguage(language: string): void {
     this.translate.use(language);
     this.selectedLang = language;
     if (this.selectedLang == 'en') {
-      this.currentLan = "English";
+      this.currentLan = 'English';
     } else if (this.selectedLang == 'si') {
-      this.currentLan = "සිංහල";
+      this.currentLan = 'සිංහල';
     } else if (this.selectedLang == 'ta') {
-      this.currentLan = "தமிழ்";
+      this.currentLan = 'தமிழ்';
     }
+    this.closeMyMenu()
   }
 
   closeMyMenu() {
-    this.trigger.closeMenu();
-  }  
+    $('.collapse').collapse('hide')
+    clearTimeout(this.timer)
+  }
 
- 
- }
-
-
+  collapseAuto() {
+    this.timer = setTimeout(this.closeMyMenu, 2000)
+  }
+}
