792f6ea67ed421e723cc7afcb03aef8fd44878b8
melkorCBA <dadcbds@gmail.com>
2020-05-03 18:49:07 +0530

    ad direct route guard

--- frontend/src/app/guards/router.guard.module.ts
+++ frontend/src/app/guards/router.guard.module.ts
@@ -1,8 +1,9 @@
+import { DirectRouteGuard } from './direct.route.guard';
 import { RouteGuardForBucket } from './bucket.route.guard';
 
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  providers: [RouteGuardForBucket],
+  providers: [RouteGuardForBucket, DirectRouteGuard],
 })
 export class RouterGuardModule {}
